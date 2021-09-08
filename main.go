48292b618825753d74069b661693c8cf9f0ee015
zhouyang <zhouyang_atwork@foxmail.com>
2021-09-09 01:28:04 +0800

    go run main.go and see the result

--- main.go
+++ main.go
@@ -2,21 +2,63 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"github.com/boltdb/bolt"
+	"log"
 )
 
-func main() {
-	blockchain := NewBlockchain()
-	blockchain.AddBlock("a")
-	blockchain.AddBlock("b")
-	blockchain.AddBlock("c")
-	for _, block := range blockchain.blocks {
-		fmt.Println(block.PreBlockHash)
-		fmt.Println(block.Data)
-		fmt.Println(block.Hash)
-		pow := NewProofOfWOrk(block)
-		fmt.Println(strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+const dbFile = "info.db"
+const bucketName = "person"
 
+func CheckError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func write() {
+	db, err := bolt.Open(dbFile, 0600, nil)
+	CheckError(err)
+	defer db.Close()
+	err = db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+		CheckError(err)
+		err = b.Put([]byte("001"), []byte("yangyang"))
+		CheckError(err)
+		err = b.Put([]byte("002"), []byte("yangyang1"))
+		CheckError(err)
+		err = b.Put([]byte("003"), []byte("yangyang2"))
+		CheckError(err)
+		return nil
+	})
+}
+func read(key string) {
+	db, err := bolt.Open(dbFile, 0600, nil)
+	CheckError(err)
+	defer db.Close()
+	err = db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(bucketName))
+		name := bucket.Get([]byte(key))
+		fmt.Println(string(name))
+		return nil
+	})
+	CheckError(err)
+}
+func scan() {
+	db, err := bolt.Open(dbFile, 0600, nil)
+	CheckError(err)
+	defer db.Close()
+	err = db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(bucketName))
+		cursor := bucket.Cursor()
+		for k, v := cursor.First(); k != nil; k, v = cursor.Next() {
+			fmt.Printf("%s,%s\n", k, v)
+		}
+		return nil
+	})
+	CheckError(err)
+}
+func main() {
+	write()
+	read("002")
+	scan()
 }
